2c23ba3f57e012fff939a6eb3f27ec1defba5a51
VitaliiDevX <vitalii.dev.x@gmail.com>
2026-06-14 11:39:41 +0300

    refactor: add expand logic and Avatar implementation for TeacherCard

--- src/components/TeacherCard/TeacherCard.tsx
+++ src/components/TeacherCard/TeacherCard.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import type { Teacher } from "../../types/teacher";
+import Avatar from "../Avatar/Avatar";
 import Icon from "../Icon/Icon";
 import css from "./TeacherCard.module.css";
 
@@ -7,6 +9,8 @@ interface Props {
 }
 
 export const TeacherCard = ({ teacher }: Props) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   const teacherMeta = [
     { label: "Lessons online", value: null, iconId: "icon-open-book" },
     { label: "Lessons done:", value: teacher.lessons_done, iconId: null },
@@ -20,11 +24,6 @@ export const TeacherCard = ({ teacher }: Props) => {
   ];
 
   const teacherInfo = [
-    {
-      label: "Speaks",
-      value: teacher.languages.join(", "),
-      isUnderlined: true,
-    },
     { label: "Lesson Info", value: teacher.lesson_info },
     { label: "Conditions", value: teacher.conditions.join(" ") },
   ];
@@ -32,10 +31,10 @@ export const TeacherCard = ({ teacher }: Props) => {
   return (
     <div className={css.card}>
       <div className={css.avatarWrapper}>
-        <img
-          src={teacher.avatar_url}
-          alt={teacher.name}
-          className={css.avatar}
+        <Avatar
+          url={teacher.avatar_url}
+          name={`${teacher.name} ${teacher.surname}`}
+          size={96}
         />
         <div className={css.statusIndicator} />
       </div>
@@ -61,31 +60,78 @@ export const TeacherCard = ({ teacher }: Props) => {
               </li>
             ))}
           </ul>
+          <Icon id="icon-heart" className={css.iconHeart} />
         </div>
+
         <h3 className={css.title}>{`${teacher.name} ${teacher.surname}`}</h3>
+
         <ul className={css.infoList}>
+          <li className={css.infoItem}>
+            <span className={css.category}>Speaks: </span>
+            <ul className={css.languagesList}>
+              {teacher.languages.map((lang) => (
+                <li key={lang}>
+                  <button className={css.langTag}>{lang}</button>
+                </li>
+              ))}
+            </ul>
+          </li>
+
           {teacherInfo.map((item, index) => (
             <li key={index} className={css.infoItem}>
-              <>
-                <span className={css.category}>{`${item.label}: `}</span>
-
-                {item.value && (
-                  <span className={item.isUnderlined ? css.underline : ""}>
-                    {`${item.value}`}
-                  </span>
-                )}
-              </>
+              <span className={css.category}>{`${item.label}: `}</span>
+              {item.value}
             </li>
           ))}
         </ul>
+        {isExpanded ? (
+          <>
+            <p className={css.experience}>{teacher.experience}</p>
 
-        <div className={css.tags}>
+            <ul className={css.reviewList}>
+              {teacher.reviews.map((review, index) => (
+                <li key={index} className={css.reviewItem}>
+                  <Avatar
+                    name={review.reviewer_name}
+                    className={css.reviewAvatar}
+                  />
+
+                  <div className={css.reviewerInfo}>
+                    <span className={css.reviewerName}>
+                      {review.reviewer_name}
+                    </span>
+                    <span className={css.reviewerRating}>
+                      <Icon id="icon-star" className={css.icon} />
+                      {review.reviewer_rating.toFixed(1)}
+                    </span>
+                  </div>
+
+                  <p className={css.comment}>{review.comment}</p>
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : (
+          <button
+            className={css.readMoreButton}
+            onClick={() => setIsExpanded(true)}
+          >
+            Read more
+          </button>
+        )}
+
+        <ul className={css.tags}>
           {teacher.levels.map((level) => (
-            <button key={level} className={`${css.tag} ${css.tagActive}`}>
-              #{level}
-            </button>
+            <li key={level}>
+              <button className={`${css.tag} ${css.tagActive}`}>
+                #{level}
+              </button>
+            </li>
           ))}
-        </div>
+        </ul>
+        {isExpanded && (
+          <button className={css.bookButton}>Book trial lesson</button>
+        )}
       </div>
     </div>
   );
