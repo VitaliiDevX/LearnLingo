2776d1ed61c225951575ba165126ae681212e382
VitaliiDevX <vitalii.dev.x@gmail.com>
2026-07-14 19:21:37 +0300

    feat: add Sonner toast to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from "react-router-dom";
+import { Toaster } from "sonner";
 import { router } from "./router";
 import { AuthProvider } from "./components/AuthProvider/AuthProvider";
 
 export default function App() {
   return (
     <AuthProvider>
+      <Toaster position="top-center" richColors />
       <RouterProvider router={router} />
     </AuthProvider>
   );
