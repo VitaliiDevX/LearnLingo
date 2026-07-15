e1f521de05831390a000aa253b5b34c28f0ec0e3
VitaliiDevX <vitalii.dev.x@gmail.com>
2026-07-15 13:02:55 +0300

    feat(TeacherList): implement pagination for teacher catalog

--- src/components/TeacherList/TeacherList.tsx
+++ src/components/TeacherList/TeacherList.tsx
@@ -1,17 +1,34 @@
 import type { Teacher } from "../../types/teacher";
-import { TeacherCard } from "../TeacherCard/TeacherCard";
+import Pagination from "../Pagination/Pagination";
+import TeacherCard from "../TeacherCard/TeacherCard";
 import css from "./TeacherList.module.css";
 
 interface Props {
   teachers: Teacher[];
+  fetchNextPage: () => void;
+  isFetchingNextPage: boolean;
+  hasNextPage: boolean;
 }
 
-export default function TeacherList({ teachers }: Props) {
+export default function TeacherList({
+  teachers,
+  fetchNextPage,
+  isFetchingNextPage,
+  hasNextPage,
+}: Props) {
   return (
-    <ul className={css.list}>
-      {teachers.map((teacher) => (
-        <TeacherCard key={teacher.id} teacher={teacher} />
-      ))}
-    </ul>
+    <div className={css.container}>
+      <ul className={css.list}>
+        {teachers.map((teacher) => (
+          <TeacherCard key={teacher._id} teacher={teacher} />
+        ))}
+      </ul>
+      {hasNextPage && (
+        <Pagination
+          fetchNextPage={() => fetchNextPage()}
+          isFetchingNextPage={isFetchingNextPage}
+        />
+      )}
+    </div>
   );
 }
