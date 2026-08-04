d0475ac223e661e32b61c22e781ccbe24ac983f4
VitaliiDevX <vitalii.dev.x@gmail.com>
2026-08-04 23:33:41 +0300

    style(components): refactor HeroImage layout for fluid scaling and responsiveness

--- src/pages/Home/components/HeroImage/HeroImage.tsx
+++ src/pages/Home/components/HeroImage/HeroImage.tsx
@@ -4,9 +4,12 @@ import css from "./HeroImage.module.css";
 export default function HeroImage() {
   return (
     <div className={css.wrapper}>
-      <img src="/images/char.png" className={css.image} />
-      <div className={css.laptop}>
-        <Icon id="icon-apple" className={css.appleIcon} />
+      <div className={css.contentGroup}>
+        <img src="/images/char.png" className={css.image} />
+
+        <div className={css.laptop}>
+          <Icon id="icon-apple" className={css.appleIcon} />
+        </div>
       </div>
     </div>
   );
