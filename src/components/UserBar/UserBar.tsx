aec845ba18d4f3b93d569bcc8d6afaf67f4f44eb
VitaliiDevX <vitalii.dev.x@gmail.com>
2026-08-05 15:55:08 +0300

    refactor(components): update UserBar component with flex direction support and click handler

--- src/components/UserBar/UserBar.tsx
+++ src/components/UserBar/UserBar.tsx
@@ -6,27 +6,36 @@ import { NavLink } from "react-router";
 import clsx from "clsx";
 import { useAuth } from "../../lib/hooks/useAuth";
 
-export default function UserBar() {
+interface Props {
+  direction?: "row" | "column";
+  onClick?: () => void;
+}
+
+export default function UserBar({ direction = "row", onClick }: Props) {
   const user = useAuthStore((state) => state.user);
 
   const { logout, isLoggingOut } = useAuth();
 
   const handleLogout = () => {
+    onClick?.();
     logout();
   };
 
   return (
-    <div className={css.wrapper}>
-      <NavLink
-        to="/favorites"
-        className={({ isActive }) =>
-          clsx(css.favButton, isActive && css.active)
-        }
-        title="Favorite teachers"
-      >
-        <Heart size={24} className={css.iconHeart} />
-      </NavLink>
-      {user && <Avatar size={48} name={user.name} />}
+    <div className={clsx(css.wrapper, css[direction])}>
+      <div className={css.contentWrapper}>
+        <NavLink
+          to="/favorites"
+          onClick={onClick}
+          className={({ isActive }) =>
+            clsx(css.favButton, isActive && css.active)
+          }
+          title="Favorite teachers"
+        >
+          <Heart size={24} className={css.iconHeart} />
+        </NavLink>
+        {user && <Avatar size={48} name={user.name} />}
+      </div>
       <button
         onClick={handleLogout}
         className={css.logout}
